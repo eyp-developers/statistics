62a37eaff504c0a7e436cca31f8228b21661cfbc
Oliver Stenbom <oliver@stenbom.eu>
2015-08-12 23:34:09 +0200

    GA-2 Documented urls.py

--- statistics/urls.py
+++ statistics/urls.py
@@ -2,6 +2,10 @@ from django.conf.urls import url
 
 from . import views
 
+#The URLs are quite important to the GA Stats system, as there's a custom API and system for accessing various forms
+#Sessions, Debates, Point Submit and Vote Submit pages are accessed using the id's of the session/committee.
+#The two special ones are the api urls that the debate and session pages use for the live reloading to function, they're linked to custom views that output some pretty JSON output. 
+
 urlpatterns = [
     url(r'^$', views.home, name='home'),
     url(r'^session/(?P<session_id>[0-9]+)/$', views.session, name='session'),
