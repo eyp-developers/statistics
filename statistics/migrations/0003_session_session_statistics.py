7392a49a35b1fc74dddbc39d552c7a6783f60787
Oliver Stenbom <oliver@stenbom.eu>
2015-08-14 13:53:36 +0200

    GA-20 Database migrations for the model changes

---
+++ statistics/migrations/0003_session_session_statistics.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('statistics', '0002_auto_20150810_1524'),
+    ]
+
+    operations = [
+        migrations.AddField(
+            model_name='session',
+            name='session_statistics',
+            field=models.CharField(default=b'SF', max_length=3, choices=[(b'S', b'Statistics Only'), (b'C', b'Point Content Only'), (b'JF', b'Single Form Statistics'), (b'SF', b'Split Form Statistics'), (b'N/A', b'No Statistics (Only Voting)')]),
+        ),
+    ]
