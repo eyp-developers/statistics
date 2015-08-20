9733157e3b8cf5bbe739cb85f121bfff6abf3d82
wolfskaempf <tom@wolfskaempf.de>
2015-08-20 22:18:29 +0200

    GA-44 separated public and private views

--- statistics/urls.py
+++ statistics/urls.py
@@ -1,31 +1,31 @@
 from django.conf.urls import url
 
-from views import views, auth, apis
+from views import views, auth, apis, public_views, protected_views
 
 #The URLs are quite important to the GA Stats system, as there's a custom API and system for accessing various forms
 #Sessions, Debates, Point Submit and Vote Submit pages are accessed using the id's of the session/committee.
 #The six special ones are the api urls that the debate and session pages use for the live reloading to function, they're linked to custom views that output some pretty JSON output.
 
 urlpatterns = [
-    url(r'^$', views.home, name='home'),
+    url(r'^$', public_views.home, name='home'),
     url(r'^login/$', auth.ga_login, name='login'),
     url(r'^logout/$', auth.ga_logout, name='logout'),
-    url(r'^create_session/$', views.create_session, name='create_session'),
-    url(r'^welcome/(?P<session_id>[0-9]+)/$', views.welcome, name='welcome'),
-    url(r'^edit/(?P<session_id>[0-9]+)/$', views.edit, name='edit'),
-    url(r'^add/(?P<session_id>[0-9]+)/$', views.add, name='add'),
-    url(r'^session/(?P<session_id>[0-9]+)/$', views.session, name='session'),
-    url(r'^session/(?P<session_id>[0-9]+)/debate/(?P<committee_id>[0-9]+)/$', views.debate, name='debate'),
-    url(r'^session/(?P<session_id>[0-9]+)/committee/(?P<committee_id>[0-9]+)/$', views.committee, name='committee'),
-    url(r'^session/(?P<session_id>[0-9]+)/point/(?P<committee_id>[0-9]+)/$', views.point, name='point'),
-    url(r'^session/(?P<session_id>[0-9]+)/point/(?P<committee_id>[0-9]+)/thanks/$', views.thanks, name='thanks'),
-    url(r'^session/(?P<session_id>[0-9]+)/vote/(?P<committee_id>[0-9]+)/$', views.vote, name='vote'),
-    url(r'^session/(?P<session_id>[0-9]+)/vote/(?P<committee_id>[0-9]+)/thanks/$', views.vote_thanks, name='vote_thanks'),
-    url(r'^session/(?P<session_id>[0-9]+)/content/(?P<committee_id>[0-9]+)/$', views.content, name='content'),
-    url(r'^session/(?P<session_id>[0-9]+)/content/(?P<committee_id>[0-9]+)/thanks/$', views.content_thanks, name='content_thanks'),
-    url(r'^session/(?P<session_id>[0-9]+)/joint/(?P<committee_id>[0-9]+)/$', views.joint, name='joint'),
-    url(r'^session/(?P<session_id>[0-9]+)/joint/(?P<committee_id>[0-9]+)/thanks/$', views.joint_thanks, name='joint_thanks'),
-    url(r'^session/(?P<session_id>[0-9]+)/manage/$', views.manage, name='manage'),
+    url(r'^create_session/$', protected_views.create_session, name='create_session'),
+    url(r'^welcome/(?P<session_id>[0-9]+)/$', protected_views.welcome, name='welcome'),
+    url(r'^edit/(?P<session_id>[0-9]+)/$', protected_views.edit, name='edit'),
+    url(r'^add/(?P<session_id>[0-9]+)/$', protected_views.add, name='add'),
+    url(r'^session/(?P<session_id>[0-9]+)/$', public_views.session, name='session'),
+    url(r'^session/(?P<session_id>[0-9]+)/debate/(?P<committee_id>[0-9]+)/$', public_views.debate, name='debate'),
+    url(r'^session/(?P<session_id>[0-9]+)/committee/(?P<committee_id>[0-9]+)/$', public_views.committee, name='committee'),
+    url(r'^session/(?P<session_id>[0-9]+)/point/(?P<committee_id>[0-9]+)/$', protected_views.point, name='point'),
+    url(r'^session/(?P<session_id>[0-9]+)/point/(?P<committee_id>[0-9]+)/thanks/$', protected_views.thanks, name='thanks'),
+    url(r'^session/(?P<session_id>[0-9]+)/vote/(?P<committee_id>[0-9]+)/$', protected_views.vote, name='vote'),
+    url(r'^session/(?P<session_id>[0-9]+)/vote/(?P<committee_id>[0-9]+)/thanks/$', protected_views.vote_thanks, name='vote_thanks'),
+    url(r'^session/(?P<session_id>[0-9]+)/content/(?P<committee_id>[0-9]+)/$', protected_views.content, name='content'),
+    url(r'^session/(?P<session_id>[0-9]+)/content/(?P<committee_id>[0-9]+)/thanks/$', protected_views.content_thanks, name='content_thanks'),
+    url(r'^session/(?P<session_id>[0-9]+)/joint/(?P<committee_id>[0-9]+)/$', protected_views.joint, name='joint'),
+    url(r'^session/(?P<session_id>[0-9]+)/joint/(?P<committee_id>[0-9]+)/thanks/$', protected_views.joint_thanks, name='joint_thanks'),
+    url(r'^session/(?P<session_id>[0-9]+)/manage/$', protected_views.manage, name='manage'),
     url(r'^api/session/(?P<session_id>[0-9]+)/$', apis.session_api, name='session_api'),
     url(r'^api/session/(?P<session_id>[0-9]+)/debate/(?P<committee_id>[0-9]+)/$', apis.debate_api, name='debate_api'),
     url(r'^api/session/(?P<session_id>[0-9]+)/vote/$', apis.session_vote_api, name='session_vote_api'),
--- statistics/views/protected_views.py
+++ statistics/views/protected_views.py
@@ -18,11 +18,6 @@ from ..models import Session, Committee, Point, ContentPoint, Vote, SubTopic, Ac
 #Importing the forms too.
 from ..forms import SessionForm,  SessionEditForm, PointForm, VoteForm, ContentForm, JointForm, ActiveDebateForm, ActiveRoundForm
 
-def home(request):
-    #All the home page needs is a list of all sessions ordered by the start date. We create the list, then the context and finally render the template.
-    latest_sessions_list = Session.objects.filter(session_is_visible=True).order_by('-session_start_date')[:20]
-    context = {'latest_sessions_list': latest_sessions_list}
-    return render(request, 'statistics/home.html', context)
 
 
 
@@ -166,48 +161,6 @@ def edit(request, session_id):
 def add(request, session_id):
     pass
 
-def session(request, session_id):
-    #The Session page uses static content and content that is constantly updated, the satic content is loaded with the view
-    #and the updating content updates with the session api, defined further down.
-
-    #The static data here is simply a list of the available committees (we can assume those don't change during live statistics)
-    #and the name and data of the session itself.
-    session_committee_list = Committee.objects.filter(session__id=session_id)[:30]
-    session = Session.objects.get(pk=session_id)
-    voting_enabled = session.session_voting_enabled
-    context = {'session_committee_list': session_committee_list, 'session_id': session_id, 'session': session, 'voting_enabled': voting_enabled}
-    return render(request, 'statistics/session.html', context)
-
-def debate(request, session_id, committee_id):
-    #Same for debates as for sessions, the only static content is the name and data of the committee and the session.
-    #The rest of the point/voting data comes through the api that can constantly be updated.
-    c = Committee.objects.get(pk=committee_id)
-    s = Session.objects.get(pk=session_id)
-    #The statistics_type will let us render different templates based on the statistics selected by the user.
-    statistics_type = s.session_statistics
-    print statistics_type
-
-    #The voting enabled option lets us change the html content and js so that the voting is not displayed.
-    voting_enabled = s.session_voting_enabled
-    context = {'committee': c, 'session': s, 'statistics_type': statistics_type, 'voting_enabled': voting_enabled}
-
-    if statistics_type == 'JF':
-        return render(request, 'statistics/joint.html', context)
-    elif statistics_type == 'SF':
-        return render(request, 'statistics/joint.html', context)
-    elif statistics_type == 'S':
-        return render(request, 'statistics/statistics.html', context)
-    elif statistics_type == 'C':
-        return render(request, 'statistics/content.html', context)
-    else:
-        pass
-
-def committee(request, session_id, committee_id):
-    #The idea is not only to have a "debate page", where you can see how many points are made during the debate of a particular resolution,
-    #but also for there to be a "committee page", where delegates can see how many points their
-    #committee has made during each debate, what was the longest time between points etc.
-    #This should be made in due time.
-    pass
 
 def point(request, session_id, committee_id):
     #The Point view handles the submission of points, both creating the form from the data given, validating the form,
