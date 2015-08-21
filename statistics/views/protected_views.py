c01fa5db408e87e1f0e48b4ba44f8bbf857a4a92
wolfskaempf <tom@wolfskaempf.de>
2015-08-21 22:25:08 +0200

    GA-3 added a new function check_authorization_and_render

--- statistics/views/protected_views.py
+++ statistics/views/protected_views.py
@@ -19,6 +19,13 @@ from ..models import Session, Committee, Point, ContentPoint, Vote, SubTopic, Ac
 #Importing the forms too.
 from ..forms import SessionForm, SessionEditForm, CommitteeForm, PointForm, VoteForm, ContentForm, JointForm, ActiveDebateForm, ActiveRoundForm
 
+# This is a central function. It replaces 'render' in cases where the user has to be authorized to view the page, not just authenticated.
+def check_authorization_and_render(request, template, context, session):
+    if request.user == session.session_admin_user or request.user.is_superuser:
+        return render(request, template, context)
+    else:
+        messages.add_message(request, messages.ERROR, 'You are not authorized to view this page. You need to log in as the ' + session.session_name + 'admin.')
+        return HttpResponseRedirect('/login/')
 
 
 
