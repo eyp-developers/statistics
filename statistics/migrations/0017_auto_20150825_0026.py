e985f99508db23c1011500b3a7dbbc437a7b01d0
wolfskaempf <tom@wolfskaempf.de>
2015-08-25 00:39:28 +0200

    GA-81 introduced some basic tests

---
+++ statistics/migrations/0017_auto_20150825_0026.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+from django.conf import settings
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('statistics', '0016_merge'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='session',
+            name='session_admin_user',
+            field=models.ForeignKey(related_name='session_admin', blank=True, to=settings.AUTH_USER_MODEL, null=True),
+        ),
+        migrations.AlterField(
+            model_name='session',
+            name='session_submission_user',
+            field=models.ForeignKey(related_name='session_submit', blank=True, to=settings.AUTH_USER_MODEL, null=True),
+        ),
+    ]
--- statistics/tests.py
+++ statistics/tests.py
@@ -1,12 +1,30 @@
+import datetime
+
 from django.test import TestCase
+from django.utils import timezone
 from django.core.urlresolvers import reverse
+from django.contrib.auth.models import AnonymousUser, User
 
 from views.public_views import home
+from .models import Session
 
 # Create your tests here.
 
+def create_session(timedelta=0, duration=10, statistics_type="JF", is_visible=True, name="Leipzig 2015", description="80th International Session of the European Youth Parliament", picture="https://upload.wikimedia.org/wikipedia/commons/c/c4/PM5544_with_non-PAL_signals.png", email="test@example.com", country="DE", color="deep-orange", admin_user=None, submission_user=None):
+    """
+    This will create a session with the above specified data and defaults.
+    """
+    start_date = timezone.now() + datetime.timedelta(days=timedelta)
+    end_date = timezone.now() + datetime.timedelta(days=timedelta + duration)
+
+    return Session.objects.create(session_name=name, session_description=description, session_picture=picture, session_email=email, session_country=country, session_start_date=start_date, session_end_date=end_date, session_statistics=statistics_type, session_color=color, session_is_visible=is_visible, session_admin_user=None, session_submission_user=None)
+
 
 class HomeViewTests(TestCase):
+
+    def setUp(self):
+        self.user = User.objects.create_user(
+            username='max', email='max@mustermann.de', password='top_secret')
 
     def test_home_view__without_any_sessions_created(self):
         """
@@ -20,4 +38,13 @@ class HomeViewTests(TestCase):
         self.assertQuerysetEqual(response.context['latest_sessions_list'], [])
 
     def test_home_view_with_one_session_created(self):
-        pass
+        """
+        This test will create a session with all the standard values and see whether it shows up properly
+        """
+        create_session()
+        response = self.client.get(reverse("statistics:home"))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "Leipzig 2015")
+        self.assertContains(response, "80th International Session of the European Youth Parliament")
+        self.assertContains(response, "Account")
+        self.assertContains(response, "Login")
