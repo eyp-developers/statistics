fee6d6ccadc34b3eb9c26c3ad14ab95cc2551e52
wolfskaempf <tom@wolfskaempf.de>
2015-08-25 12:21:36 +0200

    GA-81 added basic login view tests

--- statistics/tests.py
+++ statistics/tests.py
@@ -19,12 +19,17 @@ def create_session(timedelta=0, duration=10, statistics_type="JF", is_visible=Tr
 
     return Session.objects.create(session_name=name, session_description=description, session_picture=picture, session_email=email, session_country=country, session_start_date=start_date, session_end_date=end_date, session_statistics=statistics_type, session_color=color, session_is_visible=is_visible, session_admin_user=None, session_submission_user=None)
 
+def create_user_max():
+    """
+    This will always create the example user max.
+    """
+    return User.objects.create_user(
+        username='max', email='max@mustermann.de', password='top_secret')
 
 class HomeViewTests(TestCase):
 
     def setUp(self):
-        self.user = User.objects.create_user(
-            username='max', email='max@mustermann.de', password='top_secret')
+        self.user = create_user_max()
 
     def test_home_view__without_any_sessions_created(self):
         """
@@ -47,4 +52,17 @@ class HomeViewTests(TestCase):
         self.assertContains(response, "Leipzig 2015")
         self.assertContains(response, "80th International Session of the European Youth Parliament")
         self.assertContains(response, "Account")
+        self.assertContains(response, "Login")
+
+class LoginViewTests(TestCase):
+
+    def setUp(self):
+        self.user = create_user_max()
+
+    def test_login_view_loads_properly(self):
+        """
+        This test will see whether the login view contains the necessary elements needed to log in a user.
+        """
+        response = self.client.get(reverse("statistics:login"))
+        self.assertEqual(response.status_code, 200)
         self.assertContains(response, "Login")
