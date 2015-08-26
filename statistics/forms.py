b5317973accbb5ded4b5be8329c92fc7909d471e
wolfskaempf <tom@wolfskaempf.de>
2015-08-26 19:13:13 +0200

    GA-42 removed Crispy specific code from the LoginForm

--- statistics/forms.py
+++ statistics/forms.py
@@ -443,15 +443,3 @@ class LoginForm(forms.Form):
     # This is the form a user fills out to log in
     username = forms.CharField()
     password = forms.CharField(widget=forms.PasswordInput)
-
-
-    # Crispy Forms. This defines what the form should look like when displayed. This is specific to Bootstrap 3
-    helper = FormHelper()
-    helper.form_class = 'form-horizontal form-group'
-    helper.label_class = 'col-lg-1 form-control'
-    helper.field_class = 'col-lg-3 input-lg form-control'
-    helper.layout = Layout(
-        Field('username'),
-        Field('password'),
-        Submit('submit', 'Login', css_class='btn-default btn-lg col-lg-4 col-xs-12'),
-    )
