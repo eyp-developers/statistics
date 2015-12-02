28c9fbc8a279c71d4debf3aaeb05aa18595b8fa0
wolfskaempf <tom@wolfskaempf.de>
2015-12-03 00:20:14 +0100

    add back the previously missing static files

---
+++ statistics/static/statistics/js/auto_debate_redirect.js
@@ -0,0 +1,19 @@
+var active_debate_on_load;
+var latest_active_debate;
+
+// Here we're getting the active debate json data and assign a value to active_debate_on_load which will stay the same the entire time the pages is not refreshed
+$.getJSON(active_debate_url, function(result){
+      active_debate_on_load = result.active_debate_pk;
+  });
+
+// This will check whether the active debate has changed every five seconds and assign the value to latest_active_debate
+window.setInterval(function(){
+  $.getJSON(active_debate_url, function(result){
+        latest_active_debate = result.active_debate_pk;
+
+    });
+    // Here we compare the two values and if they differ from each other we will redirect the browser to the new active committee
+    if (latest_active_debate !== active_debate_on_load && latest_active_debate !== undefined && active_debate_on_load !== undefined) {
+      window.location.replace("../" + latest_active_debate);
+    }
+}, 5000);
