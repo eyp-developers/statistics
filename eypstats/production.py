ccc91456a8deb2a0316a0b7e74cd6b87b59c0deb
wolfskaempf <tom@wolfskaempf.de>
2016-01-30 17:27:44 +0100

    make databases setting in production.py more secure

--- eypstats/production.py
+++ eypstats/production.py
@@ -13,10 +13,6 @@ import os
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/1.8/howto/deployment/checklist/
-
 # SECURITY WARNING: keep the secret key used in production secret!
 with open('/etc/secretkey.txt') as f:
     SECRET_KEY = f.read().strip()
@@ -74,13 +70,8 @@ WSGI_APPLICATION = 'eypstats.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
-
+with open('/etc/databases.txt') as f:
+    DATABASES = f.read().strip()
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.8/topics/i18n/
