8f56ca414c7f0dd0579847f6095e2eda41d5bf48
wolfskaempf <tom@wolfskaempf.de>
2016-02-07 18:34:17 +0100

    rename welcome.html to overview.html

--- statistics/views/protected_views.py
+++ statistics/views/protected_views.py
@@ -49,7 +49,7 @@ def welcome(request, session_id):
     committees = Committee.objects.filter(session=session).order_by('committee_name')
     context = {'session': session, 'committees': committees}
 
-    return check_authorization_and_render(request, 'statistics/welcome.html', context, session)
+    return check_authorization_and_render(request, 'statistics/overview.html', context, session)
 
 
 #################
