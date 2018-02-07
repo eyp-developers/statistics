4ba10c44b6072dda3d90a79a9d88d8aa315816ce
ostenbom <oliver@stenbom.eu>
2018-02-07 15:57:03 +0000

    using django-s3-storage

--- statistics/urls.py
+++ statistics/urls.py
@@ -31,6 +31,3 @@ urlpatterns = [
 
 urlpatterns += api_urls
 urlpatterns += session_urls
-
-if settings.DEBUG is True:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
