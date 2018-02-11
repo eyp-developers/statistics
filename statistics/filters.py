d493f4837915a5cb2e420e3fd669f34c5ca4df38
ostenbom <oliver@stenbom.eu>
2018-02-11 22:54:05 +0000

    Links to statistics debates

--- statistics/filters.py
+++ statistics/filters.py
@@ -3,12 +3,10 @@ from .models import Topic, StatisticsTopicPlace, HistoricTopicPlace
 
 
 class TopicFilter(FilterSet):
-    text = CharFilter(lookup_expr='icontains')
+    text = CharFilter(label='Topic contains', lookup_expr='icontains')
     committee = CharFilter(label='Committee name', lookup_expr='icontains', method='filter_committee')
 
     def filter_committee(self, queryset, name, value):
-        print(queryset, name, value)
-
         place_ids = [topic.id for topic in StatisticsTopicPlace.objects.all()
                      if str(value).upper() in topic.committee_name().upper()]
 
--- statistics/tables.py
+++ statistics/tables.py
@@ -1,9 +1,12 @@
 import django_tables2 as tables
 from .models import Topic
+from django.utils.html import format_html, format_html_join
+from django.urls import reverse
 
 
 class TopicTable(tables.Table):
     committee = tables.Column(empty_values=())
+    used_at = tables.Column(empty_values=())
 
     def render_committee(self, record):
         places = record.topicplace_set.all()
@@ -17,8 +20,22 @@ class TopicTable(tables.Table):
 
         return committees
 
+    def render_used_at(self, record):
+        places = record.topicplace_set.all()
+        committees = []
+        for place in places:
+            if hasattr(place, 'statisticstopicplace'):
+                committees.append(place.statisticstopicplace.committee)
+
+        committee_links = format_html_join(
+                '\n', '<a class="btn btn-primary" href="{}" role="button">{}</a>',
+                ((reverse('statistics:debate', args=[c.session.id, c.id]), c.name) for c in committees)
+            )
+
+        return committee_links
+
     class Meta:
         model = Topic
         exclude = ('id',)
-        sequence = ('text', 'committee', 'type', 'area', 'difficulty')
+        sequence = ('text', 'committee', 'type', 'area', 'difficulty', 'used_at')
         template_name = 'django_tables2/bootstrap.html'
