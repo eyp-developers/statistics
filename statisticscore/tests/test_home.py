6e64b0ac2f0b244e4509e14379e02bd16f310ebe
Tom Wolfskämpf <tom@wolfskaempf.de>
2022-08-04 19:40:56 +0200

    fix tests not comparing the queryset to actual objects but to their __repr__

--- statisticscore/tests/test_home.py
+++ statisticscore/tests/test_home.py
@@ -1,10 +1,7 @@
 from django.test import TestCase
 from django.urls import reverse
-from django.contrib.auth.models import User
 
-from statisticscore.views import home
-from ..models import Session, Committee
-from .helpers import create_session, create_committee, create_user_max
+from .helpers import create_session, create_user_max
 
 
 class HomeViewTests(TestCase):
@@ -28,21 +25,21 @@ class HomeViewTests(TestCase):
         """
         This test will create a session with all the standard values and see whether it shows up properly
         """
-        create_session()
+        session_leipzig = create_session()
         response = self.client.get(reverse("statisticscore:home"))
         self.assertEqual(response.status_code, 200)
         self.assertContains(response, "Leipzig 2015")
         self.assertContains(response, "80th International Session of the European Youth Parliament")
         self.assertContains(response, "Account")
         self.assertContains(response, "Login")
-        self.assertQuerysetEqual(response.context['latest_sessions_list'], ["<Session: Leipzig 2015>"])
+        self.assertQuerysetEqual(response.context['latest_sessions_list'], [session_leipzig])
 
     def test_home_view_with_two_sessions_created(self):
         """
         This test will create two sessions with all the standard values and see whether they shows up properly
         """
-        create_session() #Creating Session 1
-        create_session(name="Hiber 2015", description="4th International Forum of EYP Spain") #Creating Session 2
+        session_leipzig = create_session() #Creating Session 1
+        session_hiber = create_session(name="Hiber 2015", description="4th International Forum of EYP Spain") #Creating Session 2
         response = self.client.get(reverse("statisticscore:home"))
         self.assertEqual(response.status_code, 200)
         self.assertContains(response, "Leipzig 2015")
@@ -51,7 +48,7 @@ class HomeViewTests(TestCase):
         self.assertContains(response, "4th International Forum of EYP Spain")
         self.assertContains(response, "Account")
         self.assertContains(response, "Login")
-        self.assertQuerysetEqual(response.context['latest_sessions_list'], ["<Session: Hiber 2015>", "<Session: Leipzig 2015>"])
+        self.assertQuerysetEqual(response.context['latest_sessions_list'], [session_hiber, session_leipzig])
 
     def test_home_view_with_one_non_public_session_created(self):
         """
@@ -71,7 +68,7 @@ class HomeViewTests(TestCase):
         This test will create one public and one non public session and test whether they show up and do not show up as expected.
         """
         create_session(is_visible=False) #Creating non public Session 1
-        create_session() # Creating public Session 1
+        session_leipzig_public = create_session() # Creating public Session 1
         response = self.client.get(reverse("statisticscore:home"))
         self.assertEqual(response.status_code, 200)
         self.assertContains(response, "What's going on in GA?")
@@ -79,4 +76,4 @@ class HomeViewTests(TestCase):
         self.assertContains(response, "80th International Session of the European Youth Parliament")
         self.assertContains(response, "Account")
         self.assertContains(response, "Login")
-        self.assertQuerysetEqual(response.context['latest_sessions_list'], [ "<Session: Leipzig 2015>"])
+        self.assertQuerysetEqual(response.context['latest_sessions_list'], [session_leipzig_public])
